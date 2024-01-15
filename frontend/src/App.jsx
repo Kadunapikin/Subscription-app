7ae5a535f83954a4d77ee968ccbf2b9b110d6c27
Kadunapikin <ibakoshi27@gmail.com>
2024-01-15 22:54:58 +0100

    add routes to App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,23 @@
-import { useState } from 'react'
-import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './layout/Home';
+import Login from './layout/Login';
+import Register from './layout/Register';
+import Success from './layout/Success';
+import Cancel from './layout/Cancel';
 
 function App() {
 
   return (
     <>
-      <div>Hello World</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />;
+        <Route path="/login" element={<Login />} />;
+        <Route path="/register" element={<Register />} />;
+        <Route path="/success" element={<Success />} />;
+        <Route path="/cancel" element={<Cancel />} />;
+      </Routes>
+    </BrowserRouter>
     </>
   )
 }
