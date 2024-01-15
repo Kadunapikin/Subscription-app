751f828d20986900bf553cae251e9d6aaad8b49a
Kadunapikin <ibakoshi27@gmail.com>
2024-01-16 00:04:37 +0100

    add register form to Register.jsx

--- frontend/src/layout/Register.jsx
+++ frontend/src/layout/Register.jsx
@@ -2,7 +2,49 @@ import React from 'react'
 
 const Register = () => {
   return (
-    <div>Register</div>
+    <div className='flex items-center w-full mx-auto h-screen diagonal-background'>
+        <form className='grid place-items-center lg:w5/12 sm:w-9/12 mx-auto border-white text-[#4f7cff] shadow-2xl rounded-3xl' action="">
+            <div className='pt-16 pb-4 text-3xl font-bold capitalize'>
+                Register To Services
+            </div>
+            {/* full name */}
+            <div className='w-full flex flex-col px-14 py-8'>
+                <label htmlFor="">Full name</label>
+                <input 
+                type="text" 
+                className='w-full border border-gray-300 rounded-lg px-3 py-3 mt-1 text-lg outline-none'
+                placeholder='your full name'
+                required
+                />
+            </div>
+            {/* email */}
+            <div className='w-full flex flex-col px-14 py-8'>
+                <label htmlFor="">Email</label>
+                <input 
+                type="email" 
+                className='w-full border border-gray-300 rounded-lg px-3 py-3 mt-1 text-lg outline-none'
+                placeholder='example@gmail.com'
+                required
+                />
+            </div>
+            {/* password */}
+            <div className='w-full flex flex-col px-14 py-8'>
+                <label htmlFor="">Password</label>
+                <input 
+                type="password" 
+                className='w-full border border-gray-300 rounded-lg px-3 py-3 mt-1 text-lg outline-none'
+                placeholder='******'
+                required
+                />
+            </div>
+            <div className='w-full flex justify-between items-center px-14 pb-8 text-[#3d5fc4]'>
+                <div>Already have an account?</div>
+                <div>
+                    <a href="/login">Login Now</a>
+                </div>
+            </div>
+        </form>
+    </div>
   )
 }
 
