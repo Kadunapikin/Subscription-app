daa19b1a314a60460b9b66fd27473d3a598c3849
Kadunapikin <ibakoshi27@gmail.com>
2024-01-17 13:55:21 +0100

    create serviceAccountKey.json file and add admin.initializeApp from firebase to server.js

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const admin = require('firebase-admin');
 const express = require('express');
+const serviceAccount = require('./serviceAccountKey.json');
 const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
@@ -9,6 +10,11 @@ const port = 5000;
 
 app.use(express.json());
 app.use(bodyParser.json());
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://stripe-subscription-ab0e1-default-rtdb.firebaseio.com"
+  });
 
 app.use(cors({
     origin: 'http://localhost:5173'
