40110e166b3b8ac42448525c0c52bb79f32f78b6
Kadunapikin <ibakoshi27@gmail.com>
2024-01-18 13:11:17 +0100

    add handlePaymentSuccess function to Success.jsx

--- frontend/src/layout/Success.jsx
+++ frontend/src/layout/Success.jsx
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
 import success from '../assets/success.jpg';
 import firebase from '../firebase/firebaseConfig';
+import { useNavigate } from 'react-router-dom';
 
 const Success = () => {
-
+  const navigate = useNavigate
   const [userId, setUserId] = useState('');
   const [sessionId, setSessionId] = useState('');
 
@@ -22,6 +23,27 @@ const Success = () => {
     })
   }, [userId, setUserId]);
 
+  const handlePaymentSuccess = () => {
+    fetch('http://localhost:5000/api/v1/payment-success', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    body: JSON.stringify({sessionId: sessionId, firebase: userId})
+    })
+    .then((res) => {
+        if (res.ok) return res.json();
+        return res.json().then(json => Promise.reject(json))
+    })
+    .then(data => {
+      console.log(data.message);
+      navigate('/');
+    })
+    .catch(err => {
+      console.log(err.error);
+    })
+  }
+
   return (
     <div className='m-0 p-0'>
       <div className='w-full min-h-[80vh] flex flex-col justify-center items-center'>
@@ -30,7 +52,8 @@ const Success = () => {
           <h3 className='text-4xl pt-8 lg:pt-0 font-bold text-center text-slate-700'>
             Payment Successful
           </h3>
-          <button className='w-40 uppercase bg-[#3d5fc4] text-white text-xl my-16 px-2 rounded'>Proceed</button>
+          <button onClick={() => handlePaymentSuccess}
+          className='w-40 uppercase bg-[#3d5fc4] text-white text-xl my-16 px-2 rounded'>Proceed</button>
         </div>
 
       </div>
